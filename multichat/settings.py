788e562cc21802b1898505e4cb5e8bf756f10e8e
Felipe Maion <felipe.maion@gmail.com>
2018-02-16 06:52:33 -0200

    Update Procfile

--- multichat/settings.py
+++ multichat/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 """
 
 import os
+from redisify import redisify
 
+CACHES = redisify(default='redis://localhost')
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
